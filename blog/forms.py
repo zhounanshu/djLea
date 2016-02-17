6ba93b6af6d92aa111a35d6b5c6fe5e09836ed85
marvin <marvin@marvins-MacBook-Pro.local>
2016-02-18 00:02:35 +0800

    add form

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        # 告诉django哪个模型会被用来创建此表单
+        model = Post
+        # 表单中出现的字段
+        fields = ('title', 'text')
